70cd42b1635550f499620df838ed59ec49b21ad6
Andre Pereira <contato.andrepsf@gmail.com>
2020-08-29 19:36:18 -0300

    Update App with new settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faBars,
+  faShoppingCart,
+  faLeaf,
+  faSeedling,
+  faTint,
+  faWater,
+  faChevronUp,
+} from '@fortawesome/free-solid-svg-icons';
+
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+import Homepage from './pages/Homepage/index.js';
+
+library.add(
+  faBars,
+  faShoppingCart,
+  faLeaf,
+  faSeedling,
+  faTint,
+  faWater,
+  faChevronUp,
+  faFacebookF,
+  faInstagram,
+  faTwitter
+);
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+
+  body {
+    background: #000;
+  }
+`;
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <GlobalStyle whiteColor />
+      <Homepage />
+    </>
+  );
 }
