6a87c820d0d7b1339ead0a961e02e3731e065579
Andre Pereira <contato.andrepsf@gmail.com>
2020-08-30 03:49:57 -0300

    Save benefits as a constant

---
+++ src/utils/constants/benefits.js
@@ -0,0 +1,20 @@
+const benefits = [
+  {
+    title: 'Orgânico',
+    icon: 'leaf',
+  },
+  {
+    title: '100% Algodão',
+    icon: 'seedling',
+  },
+  {
+    title: 'Não encolhe',
+    icon: 'tshirt',
+  },
+  {
+    title: 'Lavável',
+    icon: 'water',
+  },
+];
+
+export default benefits;
