5b3c8cd6cd2d1acca0b669416bd4357a5f0f1d97
Andre Pereira <contato.andrepsf@gmail.com>
2020-09-01 03:51:04 -0300

    Update routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage/index';
+
+import Homepage from './pages/Homepage';
+import Cart from './pages/Cart';
+import FooterSection from './components/Sections/FooterSection';
+import Header from './components/Header';
 
 export default function Routes() {
   return (
     <BrowserRouter>
+      <Header />
       <Route path="/" exact component={Homepage} />
+      <Route path="/cart" component={Cart} />
+      <FooterSection />
     </BrowserRouter>
   );
 }
