de21a0eb9eaee11dac78d4c8a90301406a9702f0
Andre Pereira <contato.andrepsf@gmail.com>
2020-09-02 03:32:30 -0300

    Comment routes for now

--- src/Routes.js
+++ src/Routes.js
@@ -5,14 +5,18 @@ import Homepage from './pages/Homepage';
 import Cart from './pages/Cart';
 import FooterSection from './components/Sections/FooterSection';
 import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Header />
-      <Route path="/" exact component={Homepage} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      {/* <Header /> */}
+      <Route path="/homepage" exact component={Homepage} />
       <Route path="/cart" component={Cart} />
-      <FooterSection />
+      {/* <FooterSection /> */}
     </BrowserRouter>
   );
 }
