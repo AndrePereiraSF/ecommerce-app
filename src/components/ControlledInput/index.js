18ce55d320a3290fb1bc369d0339e81bc34b7299
Andre Pereira <contato.andrepsf@gmail.com>
2020-09-05 19:43:31 -0300

    Update controlled input

--- src/components/ControlledInput/index.js
+++ src/components/ControlledInput/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Controller } from 'react-hook-form';
 
-import { Input } from './styles';
+import { Input, ErrorMessage } from './styles';
 
 export default function ControlledInput(props) {
-  const { type = 'text', name, control, defaultValue = '' } = props;
+  const {
+    type = 'text',
+    name,
+    control,
+    defaultValue = '',
+    rules,
+    error
+  } = props;
 
   return (
-    <Controller
-      control={control}
-      name={name}
-      defaultValue={defaultValue}
-      render={({ onChange, onBlur, value }) => 
-        <Input type={type} value={value} onChange={onChange} />
-      }
-    />
+    <>
+      <Controller
+        control={control}
+        name={name}
+        defaultValue={defaultValue}
+        render={({ onChange, onBlur, value }) => 
+          <Input type={type} value={value} onChange={onChange} onBlur={onBlur} />
+        }
+        rules={rules}
+      />
+      <ErrorMessage>{error}</ErrorMessage>
+    </>
   );
 }
--- src/components/ControlledInput/styles.js
+++ src/components/ControlledInput/styles.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
 
 export const Input = styled.input`
-  margin-bottom: 25px;
   font-size: 18px;
   height: 50px;
-  width: 200px;
+  width: 100%;
+  margin-bottom: 5px;
+  padding: 0 5px;
 
   @media (min-width: 600px) {
-    width: 400px;
     font-size: 24px;
   }
+`;
+
+export const ErrorMessage = styled.small`
+  color: red;
+  height: 24px;
 `;
