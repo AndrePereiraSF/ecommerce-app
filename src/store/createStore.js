2ada3e839c39674176c88fee42279ff54d186215
Andre Pereira <contato.andrepsf@gmail.com>
2020-09-05 19:45:25 -0300

    Add cart reducers on redux settings

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 export default (reducers, middlewares) => {
   applyMiddleware(...middlewares);
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { userReducer } from './reducers/user';
+import { cartReducer } from './reducers/cart';
 
 const reducers = {
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer
 };
 
 export const store = createStore(
