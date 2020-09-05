9fc07fe6517af2b9ad3cd390d00b0fb881510606
Andre Pereira <contato.andrepsf@gmail.com>
2020-09-05 19:42:44 -0300

    Add endpoints to register e update style

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 import {
@@ -8,41 +9,94 @@ import {
   Submit,
   Form,
   SignInContainer,
+  ErrorMessage,
 } from './styles';
 
+import { UserService } from '../../services/user';
 import Title from '../../components/Title';
 import StyledLink from '../../components/StyledLink';
 import ControlledInput from '../../components/ControlledInput';
+import Loading from '../../components/Loading';
 
 export default function Login() {
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit, errors, watch } = useForm();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const history = useHistory();
 
-  const onSubmit = data => console.log(data);
+  const onSubmit = async ({ email, password }) => {
+    try {
+      setIsLoading(true);
+
+      await UserService.signUp({ email, password });
+
+      history.push('/');
+    } catch (error) {
+      console.error(error);
+
+      setErrorMessage('Erro ao cadastrar. Tente novamente');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <RegisterPageContainer>
-      <RegisterContainer>
-        <Title>THRIFT SHOP</Title>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <InputLabel>Email</InputLabel>
-          <ControlledInput type="email" name="email" control={control} />
-          <InputLabel>Senha</InputLabel>
-          <ControlledInput type="password" name="password" control={control} />
-          <InputLabel>Confirmar senha</InputLabel>
-          <ControlledInput
-            type="password"
-            name="confirm-password"
-            control={control}
-          />
-          <Submit type="submit" value="Registrar" />
-        </Form>
-        <SignInContainer>
-          <div>Já possui uma conta?</div>
-          <StyledLink to="/login" color="#faa014">
-            Entrar
-          </StyledLink>
-        </SignInContainer>
-      </RegisterContainer>
-    </RegisterPageContainer>
+    <>
+      <RegisterPageContainer>
+        <RegisterContainer>
+          <Title>THRIFT SHOP</Title>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <InputLabel>Email</InputLabel>
+            <ControlledInput
+              type="email"
+              name="email"
+              control={control}
+              rules={{
+                required: 'Informe um email',
+              }}
+              error={errors.email && errors.email.message}
+            />
+            <InputLabel>Senha</InputLabel>
+            <ControlledInput
+              type="password"
+              name="password"
+              control={control}
+              rules={{
+                required: 'Informe uma senha',
+                minLength: {
+                  value: 8,
+                  message: 'Mín. 8 caractéres',
+                },
+              }}
+              error={errors.password && errors.password.message}
+            />
+            <InputLabel>Confirmar senha</InputLabel>
+            <ControlledInput
+              type="password"
+              name="confirm"
+              control={control}
+              rules={{
+                required: 'Confirme sua senha',
+                validate: (value) => {
+                  return (
+                    value === watch('password') || 'As senhas devem ser iguais.'
+                  );
+                },
+              }}
+              error={errors.confirm && errors.confirm.message}
+            />
+            <ErrorMessage>{errorMessage}</ErrorMessage>
+            <Submit type="submit" value="Registrar" />
+          </Form>
+          <SignInContainer>
+            <div>Já possui uma conta?</div>
+            <StyledLink to="/" color="#faa014">
+              Entrar
+            </StyledLink>
+          </SignInContainer>
+        </RegisterContainer>
+      </RegisterPageContainer>
+      {isLoading && <Loading />}
+    </>
   );
 }
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import ShopBackgroundImg from '../../assets/images/bg-shop.jpg';
 
 export const RegisterPageContainer = styled.div`
   display: flex;
@@ -6,20 +7,26 @@ export const RegisterPageContainer = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-image: url(${ShopBackgroundImg});
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 export const RegisterContainer = styled.div`
   display: flex;
+  background: #000;
   flex-direction: column;
   color: #fff;
-  padding: 50px 0;
+  padding: 15px 0;
   width: 80vw;
-  height: 85vh;
+  height: 90vh;
   justify-content: space-between;
   align-items: center;
   border: 1px solid white;
 
-  @media (min-width: 1100px) {
+  @media (min-width: 600px) {
     width: 40vw;
   }
 `;
@@ -27,6 +34,17 @@ export const RegisterContainer = styled.div`
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
+  width: 75%;
+`;
+
+export const ErrorMessage = styled.small`
+  color: red;
+  height: 20px;
+  width: 100%;
+
+  @media (min-width: 850px) {
+    font-size: 18px;
+  }
 `;
 
 export const InputLabel = styled.span`
@@ -42,16 +60,12 @@ export const Submit = styled.input`
   color: #000;
   border: 1px solid #000;
   padding: 10px;
-  margin: 20px auto 0;
+  margin: 10px auto 0;
   width: 120px;
   border-radius: 8px;
   text-align: center;
   font-size: 24px;
   cursor: pointer;
-
-  @media (min-width: 1100px) {
-    
-  }
 `;
 
 export const SignInContainer = styled.div`
